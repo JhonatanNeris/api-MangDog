1a9d8bdf4d90878205727ee1d3d75bd33e2edf82
Jhonatan <jhonatansnx@gmail.com>
2024-09-23 11:42:24 -0300

    mensagem de erro personalizada

--- src/models/Produto.js
+++ src/models/Produto.js
@@ -6,19 +6,19 @@ import { categoriaSchema } from '../models/Categoria.js'
 // Definindo o esquema para os adicionais
 const adicionalSchema = new mongoose.Schema({
     nome: { type: String, required: true },
-    descricao: { type: String},
+    descricao: { type: String },
     preco: { type: Number, required: true }
 });
 
 const produtoSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
-    nome: { type: String, required: true },
-    descricao: { type: String},
-    preco: { type: Number, required: true },
-    categoria: { type: categoriaSchema, required: true },
+    nome: { type: String, required: [true, 'O nome do produto é obrigatório'] },
+    descricao: { type: String },
+    preco: { type: Number, min: [0.1, 'O valor mínimo é R$ 0,1, R${VALUE} não é um valor válido.'], required: [true, "O preço do produto é obrigatório."] },
+    categoria: { type: categoriaSchema, required: [true, "A categoria do produto é obrigatória."] },
     adicionais: [adicionalSchema]
 }, { versionKey: false });
 
 const produto = mongoose.model("produtos", produtoSchema);
 
-export {produto, adicionalSchema, produtoSchema};
+export { produto, adicionalSchema, produtoSchema };
