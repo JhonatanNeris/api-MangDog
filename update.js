174b2515af0d5a1041b2eaac543cc93861a0050a
Jhonatan <jhonatansnx@gmail.com>
2025-07-02 13:35:30 -0300

    excluindo a connection  string do updatejs

--- update.js
+++ update.js
@@ -9,7 +9,7 @@ import configuracoes from "./src/models/Configuracoes.js";
 const clienteId = "67db8d48e70b5cdc794de4b6"; // ID do cliente que você deseja atribuir
 
 // Troque pela string real 
-const DB_CONNECTION_STRING = "mongodb+srv://admin:admin123@cluster0.19oy4.mongodb.net/mangDog?retryWrites=true&w=majority&appName=Cluster0"
+const DB_CONNECTION_STRING = ""
 
 async function atualizarItens() {
   try {
@@ -17,40 +17,18 @@ async function atualizarItens() {
       throw new Error("DB_CONNECTION_STRING não definida.");
     }
 
-    await mongoose.connect(DB_CONNECTION_STRING);
+     await mongoose.connect(DB_CONNECTION_STRING);
 
-    // buscar todos os clientes
-    const clientes = await cliente.find();
-
-    // para cada cliente, verificar se já tem configurações
-    for (const c of clientes) {
-      const jaTem = await configuracoes.findOne({ clienteId: c._id });
-      if (!jaTem) {
-        await configuracoes.create({
-          clienteId: c._id,
-          pedidos: { sequencia: 0 },
-          cardapioDigital: {
-            cardapioAtivo: false,
-            temaCardapio: {},
-            pagamento: {
-              online: {
-                ativo: false,
-                formasPagamento: []
-              },
-              local: {
-                ativo: true,
-                formasPagamento: []
-              }
-            }
-          },
-          horarioFuncionamento: {
-            dias: [],
-            diasFechadosEspeciais: []
+    await pedido.updateMany(
+      { createdAt: { $exists: false } },
+      [
+        {
+          $set: {
+            createdAt: "$horario"
           }
-        });
-        console.log(`Configuração criada para cliente ${c._id}`);
-      }
-    }
+        }
+      ]
+    );
 
     console.log(`Cliente atualizado:`);
     mongoose.connection.close();
